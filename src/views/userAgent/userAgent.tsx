eb30b21e4e6874c8b4ce9268dcf2e1dee9669e00
Ami.Raian <ami.robirai@gmail.com>
2024-10-26 15:41:10 +0600

    user agent is not solved

--- src/views/userAgent/userAgent.tsx
+++ src/views/userAgent/userAgent.tsx
@@ -6,19 +6,29 @@ import { useUserAgentContext } from "@/components/providers/userAgentProvider";
 export const UserAgent = () => {
   const { userAgent } = useUserAgentContext();
 
+  console.log("userAgent", userAgent);
+
   return (
     <div>
       <BackToHome />
 
+      {/* Content displayed if JavaScript is enabled */}
       {userAgent && (
         <div className="flex font-mono font-semibold text-sm">
           <div className="border p-2">UserAgent</div>
-
           <div className="border p-2">{userAgent}</div>
         </div>
       )}
 
       {!userAgent && <div>No user agent</div>}
+
+      {/* Fallback content when JavaScript is disabled */}
+      <noscript>
+        <div className="flex font-mono font-semibold text-sm">
+          <div className="border p-2">UserAgent</div>
+          <div className="border p-2">JavaScript is disabled, unable to fetch UserAgent</div>
+        </div>
+      </noscript>
     </div>
   );
 };
